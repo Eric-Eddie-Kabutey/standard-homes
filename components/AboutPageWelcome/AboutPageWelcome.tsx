387cfe90d32a36317bc21b54809f5511ec9e56aa
Amadou Jamanka <ajamanka72@gmail.com>
2025-02-21 16:05:09 +0000

    add About page components and images, update global styles

---
+++ components/AboutPageWelcome/AboutPageWelcome.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const AboutPageWelcome = () => {
+  return (
+    <div className="flex flex-col justify-center items-center py-16 px-6 md:px-10 w-full max-w-screen-xl mx-auto text-center">
+      <h3 className="text-xl md:text-2xl pb-6 text-[#344F64]">
+        Welcome to Waylead: Your Pathway to Exceptional Homes
+      </h3>
+      <p className="w-full sm:w-4/5 md:w-2/3 text-base md:text-lg font-semibold">
+        Founded in 2010, Waylead is a distinguished real estate developer that has been making dreams come true by crafting high-quality homes since its inception. With a passion for innovation and a commitment to excellence, we have earned a reputation as an award-winning developer, standing tall amongst the industry’s finest.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPageWelcome;
---
+++ components/NewsAndMedia/NewsAndMeida.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const properties = [
+  {
+    name: "January 9, 2025",
+    description:
+      "Waylead 2024 Workers' Retreat: Building Team Spirit and Celebrating Success.",
+  },
+  {
+    name: "October 31, 2024",
+    description: "Waylead Properties Unveil 'Willow Lane' Our Latest Housing Project.",
+  },
+  {
+    name: "October 23, 2024",
+    description: "Alphabet City: From Phase 1 Success to Phase 2 Evolution",
+  },
+];
+
+const NewsAndMedia = () => {
+  return (
+    <div className="bg-[#c9a14c] py-12 px-4 text-white">
+      <div className="container mx-auto">
+        {/* Heading */}
+        <div className="flex items-center mb-8">
+          <h1 className="text-3xl font-semibold">News & Media</h1>
+          <div className="flex-grow border-t border-[#B58541] ml-4"></div>
+        </div>
+
+        {/* Event Grid */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {properties.map((property, index) => (
+            <div
+              key={index}
+              className="bg-[#e7ac58] rounded-lg hover:bg-[#0D2A47] cursor-pointer overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105"
+            >
+              {/* Event Details */}
+              <div className="p-6 text-white">
+                <h2 className="text-2xl font-bold mb-4">{property.name}</h2>
+                <p className="text-md mb-6">{property.description}</p>
+                <Button className="bg-[#c9a14c] hover:bg-[#B58541] transition-colors duration-300">
+                  Read More &gt;
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsAndMedia;
