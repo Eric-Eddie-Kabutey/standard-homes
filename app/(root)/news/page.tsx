201ab74f16dd4c6fd4ea516324e87595c5451bdc
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-02-21 18:07:18 +0000

    Merge branch 'main' of https://github.com/CodersClutchLimited/standard-homes into news

--- app/(root)/news/page.tsx
+++ app/(root)/news/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NewsBanner from '@/components/News/NewsBanner'
+import Archive from '@/components//News/Archive'
 
 const page = () => {
   return (
     <div>
-        
+      <NewsBanner />
+      <Archive />
     </div>
   )
 }
