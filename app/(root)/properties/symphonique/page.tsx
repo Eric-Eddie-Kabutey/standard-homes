ab2c378e8a25c97af08356fcc567ec89e19a5ca5
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-02-21 21:39:48 +0000

    add OverlapCards component to display social media links for Le Jardin Symphonique

--- app/(root)/properties/symphonique/page.tsx
+++ app/(root)/properties/symphonique/page.tsx
@@ -1,9 +1,12 @@
+import OverlapCards from '@/components/Symphoni/OverlapCards'
+import { Facebook } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <OverlapCards />
     </div>
   )
 }
