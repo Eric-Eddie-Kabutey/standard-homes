c41e3f70609f045fd9cf2e9f651530a8c9904d9d
kebzin <kebbawaiga@gmail.com>
2025-02-21 16:06:00 +0000

    refactor: simplify MobileMenu component by removing unused imports

--- components/common/MobileMenue.tsx
+++ components/common/MobileMenue.tsx
@@ -1,13 +1,6 @@
 "use client";
 import React from "react";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { links } from "./Navbar";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
