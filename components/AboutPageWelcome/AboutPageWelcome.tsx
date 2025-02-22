37b84d0213d3190b7a3262ecbaf53d44efefd1cd
kebzin <kebbawaiga@gmail.com>
2025-02-22 12:05:26 +0000

    refactor: update component structure and styles in About section and NewsBanner

--- components/AboutPageWelcome/AboutPageWelcome.tsx
+++ components/AboutPageWelcome/AboutPageWelcome.tsx
@@ -8,7 +8,11 @@ const AboutPageWelcome = () => {
         Welcome to Waylead: Your Pathway to Exceptional Homes
       </h3>
       <p className="w-full sm:w-4/5 md:w-2/3 text-center text-gray-600 md:text-lg font-semibold">
-        Founded in 2010, Waylead is a distinguished real estate developer that has been making dreams come true by crafting high-quality homes since its inception. With a passion for innovation and a commitment to excellence, we have earned a reputation as an award-winning developer, standing tall amongst the industry’s finest.
+        Founded in 2010, Waylead is a distinguished real estate developer that
+        has been making dreams come true by crafting high-quality homes since
+        its inception. With a passion for innovation and a commitment to
+        excellence, we have earned a reputation as an award-winning developer,
+        standing tall amongst the industry’s finest.
       </p>
     </div>
   );
