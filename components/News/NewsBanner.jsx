8efbe9c20b4650d3eb3773937435566cee355987
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-02-24 09:44:55 +0000

    refactor: clean up NewsBanner component by removing unused markup

--- components/News/NewsBanner.jsx
+++ components/News/NewsBanner.jsx
@@ -9,12 +9,4 @@ const NewsBanner = () => {
   )
 }
 
-    <div className="w-full border-b border-slate-200 bg-gradient-to-b from-white via-[#fff] to-[#F2ECE4] h-[33vh] flex items-end">
-      <p className="mb-10 text-5xl font-extralight md:pl-[7%] text-[#1A3850]">
-        News
-      </p>
-    </div>
-  );
-};
-
-export default NewsBanner;
+export default NewsBanner
