e838778f7e2f654bba4b2316c2d86b08d7dd3d54
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-02-25 16:38:45 +0000

    feat: update AdsBanner component to replace profile editing dialog with property viewing button

--- components/Advertisements/AdsBanner.tsx
+++ components/Advertisements/AdsBanner.tsx
@@ -19,39 +19,11 @@ const AdsBanner = () => {
             <p>🚨 Limited Time Offer! Click Here for Special Discount</p>
             <Dialog>
                 <DialogTrigger asChild>
-                    <Button variant="outline">Edit Profile</Button>
+                    <Button variant="outline" className="border-none px-2 py-1 rounded-md bg-[#CD8F0F]">See property</Button>
                 </DialogTrigger>
                 <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>Edit profile</DialogTitle>
-                        <DialogDescription>
-                            Make changes to your profile here. Click save when you're done.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                                Name
-                            </Label>
-                            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="username" className="text-right">
-                                Username
-                            </Label>
-                            <Input id="username" value="@peduarte" className="col-span-3" />
-                        </div>
-                    </div>
-                    <DialogFooter>
-                        <Button type="submit">Save changes</Button>
-                    </DialogFooter>
                 </DialogContent>
             </Dialog>
-            {/* <button
-                className="border-none px-2 py-1 rounded-md bg-[#CD8F0F]"
-            >
-                See Property
-            </button> */}
         </div>
     )
 }
