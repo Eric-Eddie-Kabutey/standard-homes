cb52aee59d9acc8d56de76ada571ce5cca37def0
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-02-25 14:58:21 +0000

    feat: add ConstructionCategory and ConstructionCateory2 components with feature sections for enhanced layout and content presentation

---
+++ components/GeneralConstructiton/ConstructionCategory.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// create an inttereface for the prop
+interface Props {
+    title: string;
+}
+
+const ConstructionCategory: React.FC<Props> = ({ title }) => {
+    return (
+        <div className=''>
+            <div className="flex flex-col md:flex-row mx-[7%] items-center gap-4 mt-[2%]">
+                <h2 className="text-[#1A3850] text-[2.2rem] font-semibold mb-4">
+                    {title}
+                </h2>
+                <div className="border-b border-dashed border-slate-400 flex-1"></div>
+            </div>
+        </div>
+    );
+};
+
+export default ConstructionCategory;
---
+++ components/GeneralConstructiton/ConstructionCateory2.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+interface Props {
+    title: string;
+}
+
+const ConstructionCateory2: React.FC<Props> = ({title}) => {
+    return (
+        <div className=''>
+            <div className="flex flex-col md:flex-row mx-[7%] items-center gap-4 mb-2 mt-[2%]">
+                <div className="border-b border-dashed border-slate-400 flex-1"></div>
+                <h2 className="text-[#1A3850] font-semibold text-[2.2rem] mb-4">
+                    {title}
+                </h2>
+            </div>
+        </div>
+
+    )
+}
+
+export default ConstructionCateory2
