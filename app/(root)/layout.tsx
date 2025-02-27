02780791868928331a1ed90c44139da2337f3511
kebzin <kebbawaiga@gmail.com>
2025-02-27 15:36:55 +0000

    remove AdsBanner import from layout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
-import AdsBanner from "@/components/Advertisements/AdsBanner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
