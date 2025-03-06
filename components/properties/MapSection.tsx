24cb6dba58674288e296b2c577062592b06d49c2
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-03-06 13:23:49 +0000

    refactor: remove unused imports and commented-out code for cleaner components

--- components/properties/MapSection.tsx
+++ components/properties/MapSection.tsx
@@ -28,8 +28,8 @@ declare global {
 export default function GoogleMapComponent() {
   const [directions, setDirections] =
     useState<google.maps.DirectionsResult | null>(null);
-  const [duration, setDuration] = useState<string | null>(null);
-  const [distance, setDistance] = useState<string | null>(null);
+  // const [duration, setDuration] = useState<string | null>(null);
+  // const [distance, setDistance] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchDirections = async () => {
@@ -46,8 +46,8 @@ export default function GoogleMapComponent() {
           if (status === google.maps.DirectionsStatus.OK) {
             setDirections(result);
             const route = result?.routes[0].legs[0];
-            setDistance(route?.distance?.text || null);
-            setDuration(route?.duration?.text || null);
+            // setDistance(route?.distance?.text || null);
+            // setDuration(route?.duration?.text || null);
           } else {
             console.error("Error fetching directions:", status);
           }
@@ -70,19 +70,6 @@ export default function GoogleMapComponent() {
           <Marker position={center} />
           {directions && <DirectionsRenderer directions={directions} />}
         </GoogleMap>
-
-        {/* Card with additional information */}
-        {/* <div className="absolute top-2 right-2 bg-white p-2 shadow-lg rounded-xl ">
-          <h2 className="text-lg font-semibold">
-            Directions from Bijilo MSJ Plaza
-          </h2>
-          <p className="text-sm mt-2">Destination: Kololi</p>
-          <p className="text-sm mt-1">Distance: {distance}</p>
-          <p className="text-sm mt-1">Estimated Travel Time: {duration}</p>
-          <p className="text-xs mt-2 text-gray-500">
-            Powered by Google Maps API
-          </p>
-        </div> */}
       </div>
     </LoadScript>
   );
