498a5c22e2d3ab4b0b7bdc708cd0e8bc89a0d2d5
touraye <touraye07@gmail.com>
2025-10-07 14:28:43 +0000

    update: change link text

--- components/PropertyOptions/PropertyOptions.jsx
+++ components/PropertyOptions/PropertyOptions.jsx
@@ -13,46 +13,42 @@ export default function PropertyOptions() {
     },
   };
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      className="relative -mt-16 flex flex-col items-center justify-center p-10  bg-[#A7795D] md:p-20"
-    >
-      <div className="flex space-x-4 -mt-16">
-        <Link
-          href="#"
-          className="px-6 py-3 text-nowrap bg-[#0F2A3C] text-white rounded-lg shadow font-semibold hover:bg-blue-700"
-        >
-          List Property
-        </Link>
-        <Link
-          href="/inquire"
-          className="px-8 py-3 text-nowrap bg-[#B58541] font-semibold text-white rounded-lg shadow hover:bg-green-700"
-        >
-          Book Stay
-        </Link>
-      </div>
-      <motion.div
-        variants={containerVariants}
-        initial="hidden"
-        whileInView="show"
-        className="hidden md:grid grid-cols-1 md:grid-cols-2 gap-4 -mt-[100px] max-w-2xl w-full"
-      >
-        <OptionCard
-          title="Do you own a property?"
-          buttonText="List Property"
-          buttonColor="[#0F2A3C]"
-          url=""
-        />
-        <OptionCard
-          title="Do you need an Apartment?"
-          buttonText="Book a Stay"
-          buttonColor="[#B58541]"
-          url="/listings"
-        />
-      </motion.div>
-    </motion.div>
-  );
+		<motion.div
+			initial={{ opacity: 0, y: 50 }}
+			whileInView={{ opacity: 1, y: 0 }}
+			className='relative -mt-16 flex flex-col items-center justify-center p-10  bg-[#A7795D] md:p-20'>
+			<div className='flex space-x-4 -mt-16'>
+				<Link
+					href='#'
+					className='px-6 py-3 text-nowrap bg-[#0F2A3C] text-white rounded-lg shadow font-semibold hover:bg-blue-700'>
+					Rent / Sales
+				</Link>
+				<Link
+					href='/inquire'
+					className='px-8 py-3 text-nowrap bg-[#B58541] font-semibold text-white rounded-lg shadow hover:bg-green-700'>
+					List Property
+				</Link>
+			</div>
+			<motion.div
+				variants={containerVariants}
+				initial='hidden'
+				whileInView='show'
+				className='hidden md:grid grid-cols-1 md:grid-cols-2 gap-4 -mt-[100px] max-w-2xl w-full'>
+				<OptionCard
+					title='Do you own a property?'
+					buttonText='List Property'
+					buttonColor='[#0F2A3C]'
+					url=''
+				/>
+				<OptionCard
+					title='Do you need an Apartment?'
+					buttonText='Book a Stay'
+					buttonColor='[#B58541]'
+					url='/listings'
+				/>
+			</motion.div>
+		</motion.div>
+	)
 }
 
 function OptionCard({ title, buttonText, buttonColor, url }) {
