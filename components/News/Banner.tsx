f63ea0f8dffdb32c3bbb3fe86644bddb06a280a1
touraye <touraye07@gmail.com>
2025-10-13 10:45:22 +0000

    update: fix spacing and alignment

--- components/News/Banner.tsx
+++ components/News/Banner.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const NewsBanner = ({ title }: { title: string }) => {
   return (
-    <div className="w-full border-b border-slate-200 bg-gradient-to-b from-white via-[#fff] to-[#F2ECE4] h-[23vh] flex items-end">
-      <p className="mb-10 text-5xl font-extralight pl-[5%] md:pl-[8%] text-[#1A3850]">
-        {title}
-      </p>
+    <div className="border-b border-slate-200 bg-gradient-to-b from-white via-[#fff] to-[#F2ECE4] h-[28vh] lg:h-[50vh] 2xl:h-[35vh] flex items-end mb-10">
+      <div className="w-full max-w-[1230px] 2xl:max-w-[1390px] mx-auto px-4 pb-6">
+        <p className="text-5xl font-extralight  text-[#1A3850]">
+          {title}
+        </p>        
+      </div>
     </div>
   );
 };
